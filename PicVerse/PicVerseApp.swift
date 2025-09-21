74ae9d128de15dfa7a5297756b4244647cb0cae1
DN-Developers <dndevelopers007@gmail.com>
2025-09-21 16:48:43 +0530

    Initial Commit

---
+++ PicVerse/PicVerseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicVerseApp.swift
+//  PicVerse
+//
+//  Created by Neel Kalariya on 21/09/25.
+//
+
+import SwiftUI
+
+@main
+struct PicVerseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
